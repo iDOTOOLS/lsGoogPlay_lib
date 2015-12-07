bc90bf62523fc49da196b9a497ba2acb5207b9fa
daihuido <daihui@idotools.com>
2015-12-07 15:02:58 +0800

    d

--- src/com/lockscreen/lib/LockScreenValues.java
+++ src/com/lockscreen/lib/LockScreenValues.java
@@ -3,7 +3,7 @@ package com.lockscreen.lib;
 public class LockScreenValues {
 
     public static final String PROCESS_MASTER = "com.dotools.flashlockscreen";
-    public static final boolean isPublishInChina      = true;
+    public static final boolean isPublishInChina      = false;
     public static final boolean isIOS8LockscreenBranch = false;
 
 }
