2142bcdbe9ce5216cd6b7988f5015e6977b512d3
daihuido <daihui@idotools.com>
2015-12-11 10:12:27 +0800

    googlelay update f09

---
+++ src/com/lockscreen/lib/China/IsChina_UmengUpdate.java
@@ -0,0 +1,10 @@
+package com.lockscreen.lib.China;
+
+import android.app.Activity;
+
+public class IsChina_UmengUpdate {
+
+    public static void umengUpdateInSettingPage(Activity act) {}
+
+    public void versionUpdate(Activity act) {}
+}
