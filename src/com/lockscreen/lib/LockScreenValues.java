2d4ee11d33a5e03c398f073cbcbc6936e6b3efb9
daihuido <daihui@idotools.com>
2015-12-16 17:26:47 +0800

    add hipad branch

--- src/com/lockscreen/lib/LockScreenValues.java
+++ src/com/lockscreen/lib/LockScreenValues.java
@@ -2,8 +2,7 @@ package com.lockscreen.lib;
 
 public class LockScreenValues {
 
-    public static final String PROCESS_MASTER = "com.dotools.flashlockscreen";
     public static final boolean isPublishInChina      = false;
     public static final boolean isIOS8LockscreenBranch = false;
-
+    public static final boolean isPublishInHipad = false;
 }
